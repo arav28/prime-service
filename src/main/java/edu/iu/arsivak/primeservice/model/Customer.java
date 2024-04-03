0c8d366f81c641431883b953aa73079c34c2c087
aruve <aruve28@gmail.com>
2024-04-02 21:43:15 -0400

    practicum 12 v2----

--- src/main/java/edu/iu/arsivak/primeservice/model/Customer.java
+++ src/main/java/edu/iu/arsivak/primeservice/model/Customer.java
@@ -3,7 +3,7 @@ package edu.iu.arsivak.primeservice.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import jakarta.persistence.Id;
-import jakarta.persistence.Column;
+
 @Table
 @Entity
 public class Customer {
