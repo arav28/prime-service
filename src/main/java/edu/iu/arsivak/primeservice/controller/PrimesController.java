9131735b09248f8f13276c7c645aa3d7e21a0ed9
aruve <aruve28@gmail.com>
2024-04-09 16:28:54 -0400

    practicum 13 rabbitMQ

--- src/main/java/edu/iu/arsivak/primeservice/controller/PrimesController.java
+++ src/main/java/edu/iu/arsivak/primeservice/controller/PrimesController.java
@@ -1,7 +1,10 @@
 package edu.iu.arsivak.primeservice.controller;
 
 
+import edu.iu.arsivak.primeservice.rabbitmq.MQSender;
 import edu.iu.arsivak.primeservice.service.IprimesService;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -11,11 +14,20 @@ public class PrimesController {
 
     IprimesService iprimesService;
 
-    public PrimesController(IprimesService iprimesService) {
+    private final MQSender mqsender;
+
+    public PrimesController(IprimesService iprimesService, MQSender mqsender) {
         this.iprimesService = iprimesService;
+        this.mqsender =mqsender;
     }
     @GetMapping("/{n}")
     public boolean isPrime(@PathVariable int n) {
-        return iprimesService.isPrime(n);
+        boolean result = iprimesService.isPrime(n);
+        Object principal = SecurityContextHolder
+                .getContext().getAuthentication().getPrincipal();
+        String username = ((Jwt) principal).getSubject();
+        System.out.println(username);
+        mqsender.sendMessage(username,n,result);
+        return result;
     }
 }
